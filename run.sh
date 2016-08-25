1a4a0fd7e4cd9c0fca9aa2e8e4fd11a78cc1b382
Casey Foster <c@sey.me>
2016-08-25 16:40:39 -0500

    Use REF and SHA to tag images

--- run.sh
+++ run.sh
@@ -5,21 +5,25 @@ if [ "$BUILD_AND_PUSH_GITHUB_TOKEN" ]; then
 fi
 
 SHA=`curl -sSL https://api.github.com/repos/$GITHUB_REPO/commits/$REF$Q | jq -r .sha`
+REF_TAG=$IMAGE_REPO:$REF
+SHA_TAG=$IMAGE_REPO:$SHA
+TMP=/tmp/$SHA_TAG
 REPO_URL=https://api.github.com/repos/$GITHUB_REPO/tarball/$SHA$Q
-IMAGE=$IMAGE_REPO:$SHA
-TMP=/tmp/$IMAGE
 
 rm -fr $TMP
 mkdir -p $TMP
 curl -sSL $REPO_URL | tar xz --strip-components 1 -C $TMP
 cd $TMP
 
-echo "Building $IMAGE"
-docker build -t latest -t $IMAGE --build-arg VERSION=$SHA .
+echo "Building $REF_TAG and $SHA_TAG"
+docker build -t $REF_TAG -t $SHA_TAG --build-arg VERSION=$SHA .
 
 rm -fr $TMP
 
-echo "Pushing $IMAGE"
-docker push $IMAGE
+echo "Pushing $REF_TAG"
+docker push $REF_TAG
 
-echo "Successfully built and pushed $IMAGE"
+echo "Pushing $SHA_TAG"
+docker push $SHA_TAG
+
+echo "Successfully built and pushed $REF_TAG and $SHA_TAG"
