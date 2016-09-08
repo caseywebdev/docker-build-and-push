7086d2ca9286ffea89d96a6eb5a92d8373b045c1
Casey Foster <c@sey.me>
2016-09-08 10:57:57 -0500

    Use docker build GIT_URL

--- run.sh
+++ run.sh
@@ -2,22 +2,15 @@ set -e
 
 if [ "$BUILD_AND_PUSH_GITHUB_TOKEN" ]; then
   Q="?access_token=$BUILD_AND_PUSH_GITHUB_TOKEN"
+  CREDS="$BUILD_AND_PUSH_GITHUB_TOKEN:@"
 fi
 
 SHA=`curl -sSL https://api.github.com/repos/$GITHUB_REPO/commits/$REF$Q | jq -r .sha`
 REF_TAG=$IMAGE_REPO:$REF
 SHA_TAG=$IMAGE_REPO:$SHA
-TMP=/tmp/$SHA_TAG
-REPO_URL=https://api.github.com/repos/$GITHUB_REPO/tarball/$SHA$Q
+REPO_URL=https://${CREDS}github.com/$GITHUB_REPO.git#$SHA
 
-rm -fr $TMP
-mkdir -p $TMP
-curl -sSL $REPO_URL | tar xz --strip-components 1 -C $TMP
-cd $TMP
-
-docker build -t $REF_TAG -t $SHA_TAG --build-arg VERSION=$SHA .
-
-rm -fr $TMP
+docker build -t $REF_TAG -t $SHA_TAG --build-arg VERSION=$SHA $REPO_URL
 
 docker push $REF_TAG
 docker push $SHA_TAG
